25bbad7400031d3bf1303a7b06748c3741526f38
nightowling <hieu.nguyen@retailfoundation.de>
2023-06-13 13:41:27 +0200

    trying explicit types

--- project_a/index.ts
+++ project_a/index.ts
@@ -1,9 +1,27 @@
+import {
+  Instance,
+  IModelType,
+  ModelPropertiesDeclarationToProperties,
+  ModelPropertiesDeclaration,
+} from "mobx-state-tree";
 import { types } from "mobx-state-tree";
+// import { types } from "mobx-state-tree/dist/types";
+
 export { types };
+export type {
+  Instance,
+  IModelType,
+  ModelPropertiesDeclarationToProperties,
+  ModelPropertiesDeclaration,
+};
+export type CorrectTypes = typeof types;
+export const RealTypes: CorrectTypes = types;
 
 export const ModelA = types.model({
   a: types.optional(types.string, ""),
 });
+
+export type IModelA = Instance<typeof ModelA>;
 
 export const modelA = ModelA.create({
   a: "",
--- project_b/index.ts
+++ project_b/index.ts
@@ -1,11 +1,45 @@
-import { modelA, ModelA } from "bug-type-annotation-a";
-import { types } from "mobx-state-tree";
+import {
+  modelA,
+  ModelA,
+  RealTypes,
+  IModelA,
+  IModelType,
+  // ModelPropertiesDeclarationToProperties,
+  // ModelPropertiesDeclaration,
+} from "bug-type-annotation-a";
+// import { types } from "mobx-state-tree/dist/types";
+import { Instance, types } from "bug-type-annotation-a";
+import {
+  IAnyType,
+  IArrayType,
+  ModelPrimitive,
+  ModelPropertiesDeclaration,
+  ModelPropertiesDeclarationToProperties,
+} from "mobx-state-tree";
 
-export const ModelB = types.model({
+// type fixed = IModelType<
+//   {
+//     modelAs: IModelA;
+//   },
+//   any
+// >;
+
+export interface MyModelType extends ModelPropertiesDeclaration {
+  modelAs: IArrayType<typeof ModelA>;
+}
+
+const modelInput: MyModelType = {
   modelAs: types.array(ModelA),
-});
+};
+type TypeForModelB = IModelType<
+  ModelPropertiesDeclarationToProperties<MyModelType>,
+  {}
+>;
+export const ModelB: TypeForModelB = types.model<typeof modelInput>(modelInput);
 
-export const modelB = ModelB.create({
+type IModelB = Instance<typeof ModelB>;
+
+export const modelB: IModelB = ModelB.create({
   modelAs: [
     {
       a: "",
